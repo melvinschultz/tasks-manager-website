8e4c22dd4140e60e6853373b3430383c8c05ce9a
Melvin Schultz <melvinschultzdev@gmail.com>
2017-02-20 22:38:25 +0100

    change button add and return

--- tasks.php
+++ tasks.php
@@ -10,11 +10,11 @@ include_once 'header.php';
         <p id="error" class="error"></p>
         <div class="col-md-6 col-md-offset-3">
             <div class="todolist not-done">
-                <h1>Todos</h1>
+                <h1>Mes tâches</h1>
                 <form id="add-form" action="" method="post">
-                    <input type="text" name="task" class="form-control add-todo" placeholder="Add todo">
+                    <input type="text" name="task" class="form-control add-todo" placeholder="Ma nouvelle tâche">
                 </form>
-                <button id="addTask" class="btn btn-success">Ajouter une tâche</button>
+                <button id="addTask" class="btn btn-success" title="Ajouter une tâche"><i class="glyphicon glyphicon-plus"></i></button>
 
                 <hr>
                 <ul id="sortable" class="list-unstyled">
@@ -23,7 +23,7 @@ include_once 'header.php';
                     <strong><span class="count-todos"></span></strong> Items Left
                 </div>
             </div>
-            <a href="http://localhost/projects-oss/tasks-manager-website/login.php"><button type="button" class="return btn btn-info">Retour à la page d'authentification</button></a>
+            <a href="http://localhost/projects-oss/tasks-manager-website/login.php"><button type="button" class="return btn btn-info" title="Retour à la page d'authentification"><i class="glyphicon glyphicon-share-alt"></i></button></a>
         </div>
         <!--<div class="col-md-6">
             <div class="todolist">
